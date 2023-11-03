2bd6f93edecc5c8816c9f6bbb05ba38f7f2bfca8
Jaswanth <jaswanth@sedintechnologies.com>
2023-11-03 15:51:28 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import "./App.css";
+import TodoInput from './components/TodoInput';
+import Todolist from './components/TodoList';
 
 function App() {
+  const [listTodo, setListTodo] = useState([]);
+  const [editIndex, setEditIndex] = useState();
+  const [editInitialText, setEditInitialText] = useState("");
+
+  const addList = (inputText) => {
+    if (inputText !== "")
+      setListTodo([...listTodo, inputText]);
+  };
+
+  const deleteListItem = (key) => {
+    let newListTodo = [...listTodo];
+    newListTodo.splice(key, 1);
+    setListTodo(newListTodo);
+  };
+
+  const editListItem = (index, newText) => {
+    setEditIndex(-1);
+    setEditInitialText("");
+    let updatedList = [...listTodo];
+    updatedList[index] = newText;
+    setListTodo(updatedList);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <div className="center-container">
+        <TodoInput addList={addList} />
+        <h1 className="app-heading">TODO LIST</h1>
+        <hr />
+        {listTodo.map((listItem, i) => (
+          <Todolist
+            key={i}
+            index={i}
+            item={listItem}
+            deleteItem={deleteListItem}
+            editItem={editListItem}
+            isEditing={i === editIndex}
+            initialText={i === editIndex ? editInitialText : ""}
+            setEditIndex={setEditIndex}
+            setEditInitialText={setEditInitialText}
+          />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/components/TodoList.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+
+function Todolist(props) {
+  const [isEditing, setEditing] = useState(false);
+  const [editedItem, setEditedItem] = useState(props.item);
+
+  const handleEditClick = () => {
+    setEditing(true);
+  };
+
+  const handleSaveClick = () => {
+    props.editItem(props.index, editedItem);
+    setEditing(false);
+  };
+
+  const handleCancelClick = () => {
+    setEditing(false);
+    setEditedItem(props.item);
+  };
+
+  return (
+    <li className={`list-item`}>
+      {isEditing ? (
+        <div>
+          <input
+            type="text"
+            className="edit-input-box"
+            value={editedItem}
+            onChange={(e) => setEditedItem(e.target.value)}
+          />
+          <span className='icons'>
+            <i className="fa-solid fa-circle-check icon-save" onClick={handleSaveClick}></i>
+            <i className="fa-solid fa-circle-xmark icon-delete" onClick={handleCancelClick}></i>
+          </span>
+        </div>
+      ) : (
+        <div>
+          {props.item}
+          <span className='icons'>
+            <i className="fas fa-pencil icon-edit" onClick={handleEditClick}></i>
+            <i className="fa-solid fa-trash-can icon-delete" onClick={() => props.deleteItem(props.index)}></i>
+          </span>
+        </div>
+      )}
+    </li>
+  );
+}
+
+export default Todolist;
