9427cd3a2c2bf51235be7b176d79b69c460fa986
Jaswanth <jaswanth@sedintechnologies.com>
2023-11-06 13:19:50 +0530

    Completed list Row

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
-import "./App.css";
+import './App.css';
 import TodoInput from './components/TodoInput';
 import Todolist from './components/TodoList';
+import CompletedList from './components/CompletedList';
 
 function App() {
   const [listTodo, setListTodo] = useState([]);
   const [editIndex, setEditIndex] = useState();
-  const [editInitialText, setEditInitialText] = useState("");
+  const [editInitialText, setEditInitialText] = useState('');
+  const [completedList, setCompletedList] = useState([]);
 
   const addList = (inputText) => {
     if (inputText !== "")
@@ -14,9 +16,14 @@ function App() {
   };
 
   const deleteListItem = (key) => {
-    let newListTodo = [...listTodo];
-    newListTodo.splice(key, 1);
-    setListTodo(newListTodo);
+    const itemToDelete = listTodo[key];
+    const confirmed = window.confirm(`Are you sure you want to delete "${itemToDelete}"?`);
+    
+    if (confirmed) {
+      let newListTodo = [...listTodo];
+      newListTodo.splice(key, 1);
+      setListTodo(newListTodo);
+    }
   };
 
   const editListItem = (index, newText) => {
@@ -25,26 +32,70 @@ function App() {
     setListTodo(updatedList);
   };
 
+  const moveTaskToCompleted = (index) => {
+    const task = listTodo[index];
+    setListTodo(listTodo.filter((_, i) => i !== index));
+    setCompletedList([...completedList, task]);
+  };
+
+  const moveTaskToTodo = (index) => {
+    const task = completedList[index];
+    setCompletedList(completedList.filter((_, i) => i !== index));
+    setListTodo([...listTodo, task]);
+  };
+
+  const deleteCompletedItem = (key) => {
+    const itemToDelete = completedList[key];
+    const confirmed = window.confirm(`Are you sure you want to delete "${itemToDelete}" from the completed list?`);
+  
+    if (confirmed) {
+      let newCompletedList = [...completedList];
+      newCompletedList.splice(key, 1);
+      setCompletedList(newCompletedList);
+    }
+  };
+  
+
   return (
     <div className="main-container">
       <div className="center-container">
-        <TodoInput addList={addList} />
-        <br></br>
-        <h1 className="app-heading">TODO LIST</h1>
-        <hr />
-        {listTodo.map((listItem, i) => (
-          <Todolist
-            key={i}
-            index={i}
-            item={listItem}
-            deleteItem={deleteListItem}
-            editItem={editListItem}
-            isEditing={i === editIndex}
-            initialText={i === editIndex ? editInitialText : ""}
-            setEditIndex={setEditIndex}
-            setEditInitialText={setEditInitialText}
-          />
-        ))}
+        <div className="column">
+          
+          <TodoInput addList={addList} />
+          <br />
+          <h1 className="app-heading">TODO LIST</h1>
+          <hr />
+          {listTodo.map((listItem, i) => (
+            <Todolist
+              key={i}
+              index={i}
+              item={listItem}
+              deleteItem={deleteListItem}
+              editItem={editListItem}
+              isEditing={i === editIndex}
+              initialText={i === editIndex ? editInitialText : ''}
+              setEditIndex={setEditIndex}
+              setEditInitialText={setEditInitialText}
+              moveTaskToCompleted={moveTaskToCompleted}
+            />
+            ))}
+        </div>
+        <br />
+        <div className="column">
+          <h1 className="app-heading">Completed</h1>
+          <hr />
+          <ul>
+            {completedList.map((listItem, i) => (
+              <CompletedList
+                key={i}
+                index={i}
+                item={listItem}
+                deleteItem={deleteCompletedItem}
+                moveTaskToTodo={moveTaskToTodo}
+              />
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
